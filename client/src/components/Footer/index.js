55f1cae0d427ad009a53c2ffc93577cc49699b1b
Ewa Kubik <ewa.kubik@hotmail.com>
2022-05-12 00:48:33 +1000

    login. footer

---
+++ client/src/components/Footer/index.js
@@ -0,0 +1,23 @@
+import React from "react";
+import MailOutlineRoundedIcon from "@mui/icons-material/MailOutlineRounded";
+import {
+  TwitterIcon,
+  FacebookIcon,
+  GitHubIcon,
+  InstagramIcon,
+} from "@mui/icons-material";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div>
+        <TwitterIcon />
+      </div>
+      <div>
+        <FacebookIcon />
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
