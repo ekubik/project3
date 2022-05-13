c2accebf85dccf7b4a9b3f356d70d863d964473f
Ewa Kubik <ewa.kubik@hotmail.com>
2022-05-13 17:00:40 +1000

    rocks displayed on homepage

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,17 +1,24 @@
-import React from 'react';
-import { useQuery } from '@apollo/client';
-import "../styles/Home.css"
+import React from "react";
+import { useQuery } from "@apollo/client";
+import "../styles/Home.css";
 
-import RockList from '../components/RockList';
+import RockList from "../components/RockList";
 
-
-
-
+import { QUERY_ALL_ROCKS } from "../utils/queries";
 
 const Home = () => {
-    return (
-        <div className="homepage"><h2>I am a home page</h2> </div>
-    )
-}
+  const { loading, data } = useQuery(QUERY_ALL_ROCKS);
+  const rocks = data?.rocks || [];
+  return (
+    <div>
+      <h2> A rockin' good time awaits </h2>
+      {loading ? (
+        <div>Loading...</div>
+      ) : (
+        <RockList rocks={rocks} title="Look at all these Rocks" />
+      )}
+    </div>
+  );
+};
 
 export default Home;
