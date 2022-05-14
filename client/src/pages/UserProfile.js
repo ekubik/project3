dd00b260dbd9d33c514d6df6406ede6feaec0a00
Ewa Kubik <ewa.kubik@hotmail.com>
2022-05-14 17:50:44 +1000

    user's collection displayed on profile

--- client/src/pages/UserProfile.js
+++ client/src/pages/UserProfile.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Navigate, useParams } from "react-router-dom";
+import { Navigate, Link, useParams } from "react-router-dom";
 import { useQuery } from "@apollo/client";
 
 import { QUERY_USER, QUERY_MY_PROFILE} from "../utils/queries";
@@ -14,6 +14,8 @@ const Profile =() => {
 
     const user = data?.user || data?.myprofile|| {};
     console.log(user)
+    const rocks = user.rocks || [];
+    console.log(rocks);
 
     if (Auth.loggedIn() && Auth.getProfile().data.username === username) {
     return <Navigate to="/myprofile" />;
@@ -28,14 +30,48 @@ if (!user?.username) {
     );
 };
 
-return (<div className = "container">
-    <h2> Welcome to {username? `${user.username}'s profile`: "your" } profile! </h2>
+return (
+  <div className="container">
+    <h2> Welcome to {username ? `${user.username}'s` : "your"} profile! </h2>
+
     <div> {user.bio} </div>
-    <div> <a href={`mailto:${user.email}`}> Get in touch with {user.username} </a></div>
-    <div id="rock-collection">
-        <h2> {username? `${user.username}'s`: "Your"} Rock Collection {`(${user.rocks})`} </h2>
+    <div>
+      {" "}
+      <a href={`mailto:${user.email}`}> Get in touch with {user.username} </a>
     </div>
-</div>)
+    <div id="rock-collection">
+      <h2>
+        {" "}
+        {username ? `${user.username}'s` : "Your"} Rock Collection{" "}
+        {`(${rocks.length})`}{" "}
+      </h2>{" "}
+    </div>
+    <div>
+      {" "}
+      {username ? (
+        <br />
+      ) : (
+        <Link className="btn" to="/addRock">
+          {" "}
+          Add a Rock
+        </Link>
+      )}{" "}
+    </div>
+    <div>
+      {!rocks ? (
+        <div> This user hasn't collected any rocks yet </div>
+      ) : (
+        rocks.map((rock) => (
+          <div className="card" key={rock._id}>
+            {" "}
+            <h3> {rock.name}</h3> <p> {rock.description} </p>{" "}
+            <p> This rock was collected on {rock.dateCollected} </p>{" "}
+          </div>
+        ))
+      )}{" "}
+    </div>
+  </div>
+);
 }
 
 export default Profile;
