6dd61f208c7d3ac9e01e226a679bfc0876c7b877
Ewa Kubik <ewa.kubik@hotmail.com>
2022-05-15 16:00:19 +1000

    style sign up

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -1,12 +1,12 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import {Card, Button} from "react-bootstrap";
+import { Card, Button } from "react-bootstrap";
 
 import { useMutation } from "@apollo/client";
 import { NEW_USER } from "../utils/mutations";
 
 import Auth from "../utils/auth";
-import "../styles/SignUp.css"
+import "../styles/SignUp.css";
 
 const SignUp = () => {
   const [formState, setFormState] = useState({
@@ -39,116 +39,119 @@ const SignUp = () => {
       Auth.login(data.newUser.token);
     } catch (error) {
       console.error(error);
+      alert("Something went wrong. Please try again.")
     }
   };
 
   return (
-    <div className="container">
-      <div className="flex-row justify-center">
-        <Card className="text-center">
-          <div>
-            <Card.Header>
-              <h1> SIGN UP </h1>
-            </Card.Header>
-          </div>
-          <div className="card-body">
-            <h2>
-              Already have an account?{" "}
-              <Link className="redirectLink" to="/login">
-                Click here to log in
-              </Link>
-            </h2>
-            {data ? (
-              <p>
-                Success! You may now head{" "}
-                <Link className="redirectLink" to="/">
-                  back to the homepage.
+    <div className="container-fluid main">
+      <div className="row justify-content-center cardBody">
+        <div className="col-8">
+          <Card className="text-center">
+            <div>
+              <Card.Header className="cardHeader">
+                <h1> SIGN UP </h1>
+              </Card.Header>
+            </div>
+            <div className="card-body">
+              <h2>
+                Already have an account?{" "}
+                <Link className="redirectLink" to="/login">
+                  Click here to log in
                 </Link>
-              </p>
-            ) : (
-              <form onSubmit={handleFormSubmit}>
-                <div className="row justify-content-center">
-                  <label className="col-3" htmlFor="firstName">
-                    {" "}
-                    First Name{" "}
-                  </label>
-                  <input
-                    autoComplete="on"
-                    className="form-input col-3"
-                    placeholder="First Name"
-                    name="firstName"
-                    type="text"
-                    value={formState.firstName}
-                    onChange={handleInputChange}
-                  ></input>
-                </div>
-                <div className="row justify-content-center">
-                  <label className="col-3" htmlFor="lastName">
-                    {" "}
-                    Last Name{" "}
-                  </label>
-                  <input
-                    className="form-input col-3"
-                    placeholder="Last Name"
-                    name="lastName"
-                    type="text"
-                    value={formState.lastName}
-                    onChange={handleInputChange}
-                  ></input>
-                </div>
-                <div className="row justify-content-center">
-                  <label className="col-3" htmlFor="username">
-                    {" "}
-                    Username:{" "}
-                  </label>
-                  <input
-                    className="form-input col-3"
-                    placeholder="Username"
-                    autoComplete="username"
-                    name="username"
-                    type="text"
-                    value={formState.username}
-                    onChange={handleInputChange}
-                  ></input>
-                </div>
-                <div className="row justify-content-center">
-                  <label className="col-3" htmlFor="email">
-                    {" "}
-                    Email{" "}
-                  </label>
-                  <input
-                    className="form-input col-3"
-                    placeholder="Email"
-                    autoComplete="email"
-                    name="email"
-                    type="text"
-                    value={formState.email}
-                    onChange={handleInputChange}
-                  ></input>
-                </div>
-                <div className="row justify-content-center">
-                  <label className="col-3" htmlFor="password">
-                    {" "}
-                    Password{" "}
-                  </label>
-                  <input
-                    className="form-input col-3"
-                    placeholder="*******"
-                    name="password"
-                    type="password"
-                    autoComplete="new-password"
-                    value={formState.password}
-                    onChange={handleInputChange}
-                  ></input>
-                </div>
-                <button className="submitButton"> Submit </button>
-              </form>
-            )}
-          </div>
-        </Card>
-        {error && (
-          <div className="my-3 p-3 bg-danger text-white">{error.message}</div>
-        )}
+              </h2>
+              {data ? (
+                <p>
+                  Success! You may now head{" "}
+                  <Link className="redirectLink" to="/">
+                    back to the homepage.
+                  </Link>
+                </p>
+              ) : (
+                <form onSubmit={handleFormSubmit}>
+                  <div className="row justify-content-center">
+                    <label className="col-3" htmlFor="firstName">
+                      {" "}
+                      First Name{" "}
+                    </label>
+                    <input
+                      autoComplete="on"
+                      className="form-input col-3"
+                      placeholder="First Name"
+                      name="firstName"
+                      type="text"
+                      value={formState.firstName}
+                      onChange={handleInputChange}
+                    ></input>
+                  </div>
+                  <div className="row justify-content-center">
+                    <label className="col-3" htmlFor="lastName">
+                      {" "}
+                      Last Name{" "}
+                    </label>
+                    <input
+                      className="form-input col-3"
+                      placeholder="Last Name"
+                      name="lastName"
+                      type="text"
+                      value={formState.lastName}
+                      onChange={handleInputChange}
+                    ></input>
+                  </div>
+                  <div className="row justify-content-center">
+                    <label className="col-3" htmlFor="username">
+                      {" "}
+                      Username:{" "}
+                    </label>
+                    <input
+                      className="form-input col-3"
+                      placeholder="Username"
+                      autoComplete="off"
+                      name="username"
+                      type="text"
+                      value={formState.username}
+                      onChange={handleInputChange}
+                    ></input>
+                  </div>
+                  <div className="row justify-content-center">
+                    <label className="col-3" htmlFor="email">
+                      {" "}
+                      Email{" "}
+                    </label>
+                    <input
+                      className="form-input col-3"
+                      placeholder="Email"
+                      autoComplete="email"
+                      name="email"
+                      type="text"
+                      value={formState.email}
+                      onChange={handleInputChange}
+                    ></input>
+                  </div>
+                  <div className="row justify-content-center">
+                    <label className="col-3" htmlFor="password">
+                      {" "}
+                      Password{" "}
+                    </label>
+                    <input
+                      className="form-input col-3"
+                      placeholder="*******"
+                      name="password"
+                      type="password"
+                      autoComplete="new-password"
+                      value={formState.password}
+                      onChange={handleInputChange}
+                    ></input>
+                  </div>
+                  <button className="submitButton"> Submit </button>
+                </form>
+              )}
+            </div>
+          </Card>
+          {error && (
+            <div className="my-3 p-3 bg-danger text-white">{error.message}</div>
+          )}
+        </div>
       </div>
     </div>
   );
